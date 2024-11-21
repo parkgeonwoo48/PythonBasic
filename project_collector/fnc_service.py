d56b43b7637a951c0fcae638ce8181d82a629e3e
parkgeonwoo <67894341+parkgeonwoo48@users.noreply.github.com>
2024-11-21 17:08:36 +0900

    Add files via upload

--- project_collector/fnc_service.py
+++ project_collector/fnc_service.py
@@ -3,26 +3,17 @@ from bs4 import BeautifulSoup
 from collect_dao import insert_news
 
 
-def collect_news(category:str):
-    page = 1
-    count = 1
-    while True:
-        url = f"https://news.daum.net/breakingnews/{category}?page={page}"
-        result = requests.get(url) 
-        doc = BeautifulSoup(result.text, "html.parser")
-
-        link_list = doc.select("ul.list_news2 a.link_txt")
-    
-    
-        if len(link_list) == 0:
-            break
-
-        for  link in link_list:
-            print(f"{count+1} ================================================================")
-            get_news_info(link["href"])
-            count+=1
-        page+=1
-
+def collect_news():
+    count = 1  
+    url = "https://news.daum.net/home"
+    result = requests.get(url)
+    doc = BeautifulSoup(result.text, "html.parser")
+    link_list = doc.select("ul.list_newsbasic a.item_newsbasic")
+    for link in link_list:
+        print(f"{count} ===============================================")
+        get_news_info(link["href"])
+        count+=1
+       
 
 def get_news_info(url: str):
     result = requests.get(url)
@@ -31,27 +22,20 @@ def get_news_info(url: str):
     contents = doc.select("section > p")
     content = ""
     for text in contents:
-        
         content += text.get_text()
-   
     writer_list = doc.select("span.txt_info")
-    
     if len(writer_list) < 2:
         writer = ""
     else:
         writer = writer_list[0].get_text()
-        
-    
-    writer = doc.select("span.txt_info")[0].get_text()
     reg_date = doc.select("span.num_date")[0].get_text()
-    list_date = reg_date.split(".")
-    list_date = list(map(lambda x: x.strip(), list_date))
+    list_date = reg_date.split(". ")
     reg_date = list_date[0] + list_date[1] + list_date[2]
     print(f"뉴스 제목: {title}")
     print(f"뉴스 기자: {writer}")
-    print(f"뉴스 날짜: {reg_date}") 
-    print(f"뉴스 본문: {contents}")
-    
+    print(f"뉴스 날짜: {reg_date}")  
+    print(f"뉴스 본문: {content}")
+   
     data = {
         "title": title,
         "writer": writer,
--- project_collector/main.py
+++ project_collector/main.py
@@ -4,12 +4,24 @@
 #  3. 화면: 출력, 엑셀 다운로드(streamlit, pandas)
 #  4. 저장: 데이터베이스 저장(pymysql + MariaDB)
 from fnc_service import collect_news
+import streamlit as st
 
-category = "digital" #IT
-
+# streamlit run project_collector/main.py
+# Streamlit docs → https://docs.streamlit.io/
+# Emoji → https://snskeyboard.com/emoji/
 def main():
-    print("Start collector")
-    collect_news(category)
+    st.set_page_config(
+        page_title="뉴스 수집기",  # 제목
+        page_icon="project_collector/img/favicon.png"              # 파비콘
+    )
+    st.title("NEWS :red[COLLECTOR]")
+    st.text("실시간 뉴스를 수집합니다.")
+    with st.form(key="form"):
+        submitted = st.form_submit_button("수집")        
+   
+    # 버튼 이벤트 → 수집버튼을 클릭했을 때
+    if submitted:
+        collect_news()
 
 if __name__ == "__main__":
     main()
