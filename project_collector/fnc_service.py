85419f841e4e354b0b81ca68322db07439af4bf3
parkgeonwoo <67894341+parkgeonwoo48@users.noreply.github.com>
2024-11-28 17:14:19 +0900

    Add files via upload

--- project_collector/fnc_service.py
+++ project_collector/fnc_service.py
@@ -1,20 +1,67 @@
 import requests
 from bs4 import BeautifulSoup
 from collect_dao import insert_news
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+import pandas as pd
+
+
+options = Options()
+# options.add_experimental_option("detach",True) 
+options.add_argument("headless") 
+options.add_argument("disable-blink-features=AutomationControlled")
+options.add_experimental_option("useAutomationExtension", False)
+options.add_experimental_option("excludeSwitches",["enable-automation"])
 
 
 def collect_news():
-    count = 1  
+    count = 0 
     url = "https://news.daum.net/home"
-    result = requests.get(url)
-    doc = BeautifulSoup(result.text, "html.parser")
-    link_list = doc.select("ul.list_newsbasic a.item_newsbasic")
-    for link in link_list:
-        print(f"{count} ===============================================")
-        get_news_info(link["href"])
-        count+=1
-       
-
+    driver = webdriver.Chrome(options = options)
+    driver.get(url)
+    time.sleep(5)
+    # 숙제 중복 방지 체크
+    # 1P(9개기사) -> total 3P(27개 기사) -> 1번 수집(27개 기사)
+    # 로직
+    # 1. 기사 1건 수집 완료 -> 해당 기사 URL을 url_list에 저장
+    
+    #  -> 있음(중복), 수집 멈춤
+    #  -> 없음, 수집 계속
+    # 2. 다음 기사 수집 URL과 url_list의 URL을 비교
+    url_list = [] # 수집이 완료된 Link(URL)을 저장
+    flag = False
+    news_list = [] # 
+    while True:
+    # 9건의 기사 수집
+        doc = BeautifulSoup(driver.page_source, "html.parser")
+        link_list = doc.select("article.content-article ul.list_newsheadline2 a.item_news_headline2")
+        for link in link_list:
+            if link in url_list:
+                flag = True
+                break
+            print(f"{count} ===============================================")
+            news = get_news_info(link["href"])
+            news_list.append(news) # 수집한 데이터 로컬 저장
+            count+=1
+            url_list.append(link) # 중복수집 방지를 위한 처리
+        if flag:
+            break
+        # [새로운 뉴스] 버튼 클릭 이벤트
+        driver.find_element(By.XPATH,'//*[@id="58d84141-b8dd-413c-9500-447b39ec29b9"]/div[2]/a').click()
+        time.sleep(5)
+    # 수집한 데이터 27건 main으로 전달
+    # 1. DB활용
+    #   DB(27건 저장) -> SELECT 가져와서 전달
+    # 2. 수집시 수집데이터를 따로 저장 후 전달
+    #  - 
+    # news_list -> [{news1}, {news2}, {news3},...,{news27}]
+    # "pandas"의 DataFrame(표) Type으로 News_list저장
+    col_name = ["title", "writer","content","regdate"]
+    df_news = pd.DataFrame(news_list, columns=col_name) # DataFrame(표) 반환
+    return df_news, count
 def get_news_info(url: str):
     result = requests.get(url)
     doc = BeautifulSoup(result.text, "html.parser")
@@ -43,3 +90,5 @@ def get_news_info(url: str):
         "regdate": reg_date,
     }
     insert_news(data)
+    return data
+
--- project_collector/main.py
+++ project_collector/main.py
@@ -3,9 +3,21 @@
 #  2. 수집: 데이터 수집(requests, beautifulsoup)
 #  3. 화면: 출력, 엑셀 다운로드(streamlit, pandas)
 #  4. 저장: 데이터베이스 저장(pymysql + MariaDB)
+
+# 데이터를 수집하는 이유 ?
+# -> 데이터 분석 or 인공지능 학습
+# -> CSV or JSON 수집
+# CSV 예: 1, 최체리, 10, 여
+# JSON 예
+#{
+#   "번호":1
+#   "이름":2
+#   "나이":3
+#   "성별":여    
+#}
 from fnc_service import collect_news
 import streamlit as st
-
+from datetime import datetime
 # streamlit run project_collector/main.py
 # Streamlit docs → https://docs.streamlit.io/
 # Emoji → https://snskeyboard.com/emoji/
@@ -16,12 +28,23 @@ def main():
     )
     st.title("NEWS :red[COLLECTOR]")
     st.text("실시간 뉴스를 수집합니다.")
-    with st.form(key="form"):
-        submitted = st.form_submit_button("수집")        
-   
-    # 버튼 이벤트 → 수집버튼을 클릭했을 때
-    if submitted:
-        collect_news()
-
+    flag = False
+    if st.button("수집"):
+        df_news, count = collect_news()
+        st.write(f"뉴스 {count}건 수집 완료!")
+        st.write(df_news)
+        flag = True
+        news_csv = df_news.to_csv(index = False, encoding="utf8")
+        # df_news : DataFrame -> CSV type
+    if flag:
+        now = datetime.now().strftime("%Y%m%d%H%M%S")
+        st.download_button(
+            label="다운로드",
+            data=news_csv,
+            file_name=f"실시간뉴스_{now}.csv",
+            mime="text/csv",
+            key="download_csv"
+        )
+        
 if __name__ == "__main__":
     main()
